1fb83650c37863d5bc8690cbdae19f86053f5c48
Raul Carreras <raul.e.carreras@gmail.com>
2020-03-16 18:42:01 -0400

    deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: `http://localhost:8000/`,
-
+    API_ENDPOINT: `http://https://young-shore-13224.herokuapp.com/`,
+    API_KEY: process.env.REACT_APP_API_KEY,
 }
 
